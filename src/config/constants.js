ccd953d7e3ec6685d8475d83f5887a9d609275e5
Jesse Schulman <jesse@caleos.io>
2019-06-16 12:51:32 -0700

    Check in constants file

---
+++ src/config/constants.js
@@ -0,0 +1,18 @@
+//export const FORM_SUBMIT_ENDPOINT = 'http://64.38.144.184:4001/api/v1/rewards/create';
+export const FORM_SUBMIT_ENDPOINT = 'https://rewards.telosfoundation.io/api/v1/rewards/create';
+//export const FORM_SUBMIT_ENDPOINT = 'https://js9f0cx73l.execute-api.us-east-1.amazonaws.com/dev/api/v1/rewards/create';
+//export const FORM_SUBMIT_ENDPOINT = 'http://localhost:8080/api/v1/rewards/create';
+
+
+export const REWARDS_SUBMISSION_ENDPOINT = 'https://rewards.telosfoundation.io/api/v1/submission/create';
+
+//export const RECOVERY_FORM_ENDPOINT = 'http://localhost:8080/api/v1/recovery/create';
+export const RECOVERY_FORM_ENDPOINT = 'https://xgep1snyvg.execute-api.us-east-1.amazonaws.com/dev/api/v1/recovery/create';
+
+
+export const WHITE_PAPER_ENGLISH = 'https://resources.telosfoundation.io/wp_master/telos_white_paper_english.pdf';
+export const WHITE_PAPER_KOREAN = 'https://resources.telosfoundation.io/wp_master/텔로스_white_paper_korean.pdf';
+export const WHITE_PAPER_CHINESE = 'https://resources.telosfoundation.io/wp_master/Telos_White_Paper_8_15_18_Chinese_Version_橡果白皮书中文版.pdf';
+export const WHITE_PAPER_RUSSIAN = 'https://resources.telosfoundation.io/wp_master/telos_white_paper_russian.pdf';
+export const WHITE_PAPER_PORTUGESE = 'https://resources.telosfoundation.io/wp_master/telos_white_paper_portugese.pdf';
+export const WHITE_PAPER_INDONESIAN = 'https://resources.telosfoundation.io/wp_master/White Paper Telos Indonesian.pdf';
