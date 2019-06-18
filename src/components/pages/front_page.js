9f011f69f97d2560cbf5d7d9a398c0de5fae1607
DonaldPeat <don@caleos.io>
2019-06-17 21:55:11 -0700

    fix: remove 'partners' bp section from homepage

--- src/components/pages/front_page.js
+++ src/components/pages/front_page.js
@@ -12,47 +12,6 @@ import icon_dpos from '../../img/Telos_MarketingSite_Icon_dpos_200px.png';
 import icon_governance from '../../img/Telos_MarketingSite_Icon_governance_200px.png';
 import white_paper from '../../img/white_paper_pdf_solid.png';
 
-//partners logos
-import eos_detroit from '../../img/partner_logos/logo_eosdetroit_small.png';
-import eos_metal from '../../img/partner_logos/logo_eosmetal.png';
-import dutcheos from '../../img/partner_logos/logo_dutch_eos.png';
-import telosuk from '../../img/partner_logos/logo_telos_uk.png';
-import eosbarcelona from '../../img/partner_logos/logo_eosbarcelona.png';
-// import eosvan from '../../img/partner_logos/logo_eosvan.png';
-import goodblock from '../../img/partner_logos/logo_goodblock.png';
-import infinitybloc from '../../img/partner_logos/logo_infinitybloc.png';
-// import teloslabs from '../../img/partner_logos/logo_teloslabs.png';
-import eosworld from '../../img/partner_logos/logo_eosworld.png';
-// import eosgreen from '../../img/partner_logos/logo_eosgreen.png';
-import telos_green from '../../img/partner_logos/logo_telos_green.png';
-import fuzzy from '../../img/partner_logos/logo_fuzzy.png';
-import caleos from '../../img/partner_logos/logo_caleos.png';
-import eos_in_space from '../../img/partner_logos/logo_eosspace.png';
-// import eos_sweden from '../../img/partner_logos/logo_eossweden.png';
-import sweden_cornet from '../../img/partner_logos/logo_swedencornet.png';
-// import eos_miami from '../../img/partner_logos/logo_eosmiami.png';
-import telos_miami from '../../img/partner_logos/logo_telos_miami.png';
-import hellenicos from '../../img/partner_logos/logo_hellenicos.png';
-import discover_telos from '../../img/partner_logos/logo_discovertelos.png';
-import zmeta from '../../img/partner_logos/logo_zmeta.png';
-import blindbloc from '../../img/partner_logos/logo_blindbloc.png';
-import csx from '../../img/partner_logos/logo_csx.png';
-import telos_russia from '../../img/partner_logos/logo_telosrussia.png';
-import amplified from '../../img/partner_logos/logo_amplified.png';
-import venezuela from '../../img/partner_logos/logo_telos_venezuela.png';
-import madrid from '../../img/partner_logos/logo_telos_madrid.png';
-import telos_ny from '../../img/partner_logos/logo_telos_ny.png';
-import telos_dac from '../../img/partner_logos/logo_telos_dac.png';
-import telos_canton from '../../img/partner_logos/logo_telos_canton.png';
-import eos_impera from '../../img/partner_logos/logo_eosimpera.png';
-import zephyr from '../../img/partner_logos/logo_zephyr.png';
-import telos_vancouver from '../../img/partner_logos/logo_telos_vancouver.png';
-import kainos from '../../img/partner_logos/logo_kainos.png';
-import telos_global from '../../img/partner_logos/logo_telosglobal.png';
-import telescope from '../../img/partner_logos/logo_telescope.png';
-import eos_sphere from '../../img/partner_logos/logo_eos_sphere.png';
-import telos_awake from '../../img/partner_logos/logo_telos_awake.png';
-
 import chainrift_button from '../../img/exchange_logos/Chainrift-Button.jpg';
 import codex_button from '../../img/exchange_logos/codex_button.jpg';
 import akdex_button from '../../img/exchange_logos/akdex.png';
@@ -73,7 +32,7 @@ import carbon_logo from '../../img/stable_coin/CarbonLogo.png';
 import math_logo from '../../img/app_icon_rounded_1024.png';
 
 import splash_expo from '../../img/banners/SplashBanner_BlockchainExpo.png';
-import splash_hacker from '../../img/banners/SplashBanner_ReadyHackerOne.png'
+import splash_hacker from '../../img/banners/SplashBanner_ReadyHackerOne.png';
 import splash_exchange from '../../img/banners/SplashBanner_Exchange.png';
 import splash_wallets from '../../img/banners/SplashBanner_Wallets.jpg';
 //import splash_one from '../../img/banners/SplashBanner_Telos-Medium_generic_1_display.jpg';
@@ -99,174 +58,6 @@ import chart_rn from '../../img/charts/Telos_MarketingSite_Chart_1000px_RN1.png'
 import chart_sp from '../../img/charts/Telos_MarketingSite_Chart_1000px_SP1.png';
 
 import '../../styles/front_page.css';
-
-const partnersArr = [
-	{
-		name: 'eos detroit',
-		logo: eos_detroit,
-		url: 'https://eosdetroit.io/'
-	},
-	{
-		name: 'eos metal',
-		logo: eos_metal,
-		url: 'https://eosmetal.io'
-	},
-	{
-		name: 'telos uk',
-		logo: telosuk,
-		url: 'https://telosuk.io'
-	},
-	{
-		name: 'dutch eos',
-		logo: dutcheos,
-		url: 'https://dutcheos.io'
-	},
-	{
-		name: 'eos barcelona',
-		logo: eosbarcelona,
-		url: 'http://eosbarcelona.com/'
-	},
-	{
-		name: 'telos vancouver',
-		logo: telos_vancouver,
-		url: 'https://www.telosvancouver.io/'
-	},
-	{
-		name: 'good block',
-		logo: goodblock,
-		url: 'http://goodblock.io'
-	},
-	{
-		name: 'infinitybloc',
-		logo: infinitybloc,
-		url: 'https://www.infinitybloc.io/'
-	},
-	{
-		name: 'Eos World',
-		logo: eosworld,
-		url: 'https://eostheworld.io/'
-	},
-	{
-		name: 'telosgreen',
-		logo: telos_green
-	},
-	{
-		name: 'fuzzy',
-		logo: fuzzy
-	},
-	{
-		name: 'caleos',
-		logo: caleos,
-		url: 'https://caleos.io/'
-	},
-	{
-		name: 'eos in space',
-		logo: eos_in_space,
-		url: 'https://eosin.space/#telos'
-	},
-	{
-		name: 'telos miami',
-		logo: telos_miami,
-		url: 'http://telos.miami'
-	},
-	{
-		name: 'eos sweden',
-		logo: sweden_cornet,
-		url: 'https://eossweden.eu/'
-	},
-	{
-		name: 'octagon labs',
-		logo: hellenicos,
-		url: 'https://octagonlabs.org'
-	},
-	{
-		name: 'discover telos',
-		logo: discover_telos
-	},
-	{
-		name: 'z meta',
-		logo: zmeta
-	},
-	{
-		name: 'blind bloc',
-		logo: blindbloc,
-		url: 'https://www.blindblocart.io/'
-	},
-	{
-		name: 'eos csx',
-		logo: csx,
-		url: 'https://www.csx.io'
-	},
-	{
-		name: 'telos russia',
-		logo: telos_russia,
-		url: 'https://telosrussia.io'
-	},
-	{
-		name: 'telos amplified',
-		logo: amplified,
-		url: 'https://amplified.software/'
-	},
-	{
-		name: 'telos venezuela',
-		logo: venezuela,
-		url: 'https://www.telosvenezuela.com'
-	},
-	{
-		name: 'telos madrid',
-		logo: madrid,
-		url: 'https://telosmadrid.io/'
-	},
-	{
-		name: 'telos dac',
-		logo: telos_dac,
-		url: 'https://telosdac.io'
-	},
-	{
-		name: 'telos ny',
-		logo: telos_ny,
-		url: 'http://nytelos.com'
-	},
-	{
-		name: 'telos canton',
-		logo: telos_canton,
-		url: 'https://teloscanton.io'
-	},
-	{
-		name: 'eos impera',
-		logo: eos_impera,
-		url: 'https://eosimpera.io'
-	},
-	{
-		name: '21 zephyr',
-		logo: zephyr,
-		url: 'https://telos-21zephyr.com'
-	},
-	{
-		name: 'kainos',
-		logo: kainos
-	},
-	{
-		name: 'TelosGlobal',
-		logo: telos_global,
-		url: 'https://telosglobal.io'
-	},
-	{
-		name: 'the teloscope',
-		logo: telescope,
-		url: 'https://theteloscope.io'
-	},
-	{
-		name: 'eosphere',
-		logo: eos_sphere,
-		url: 'https://eosphere.io'
-	},
-	{
-		name: 'telos awake',
-		logo: telos_awake,
-		url: 'https://eosawake.io'
-	}
-];
 
 const exchangesArr = [
 	{
@@ -370,8 +161,7 @@ class FrontPage extends Component {
 				gov_content,
 				white_paper_heading,
 				white_paper_content,
-				github_text,
-				partners_heading } = this.props.landing_page;
+				github_text } = this.props.landing_page;
 		const {language} = this.props;
 
 		return (
@@ -395,8 +185,6 @@ class FrontPage extends Component {
 				<Exchanges />
 				<StableCoin />
 				<Wallets />
-				<Partners
-					partners_heading={partners_heading} />
 			</div>
 		);
 	}
@@ -811,40 +599,6 @@ const Wallets = () => {
 						</footer>
 					</Col>
 				</Row>
-			</Grid>
-		</section>
-	);
-};
-
-const Partners = ({partners_heading}) => {
-	return (
-		<section id='partners'>
-			<header className='text-center'>
-				<h1>{partners_heading}</h1>
-			</header>
-			<Grid>
-				<div className='partners_container'>
-					{partnersArr.map((partner, i) => {
-						return (
-							<div className='partner' key={i}>
-								<ScrollAnimation
-									animateOnce
-									animateIn='fadeIn'
-									duration={0.1}
-									delay={i * 10}
-								>
-									{partner.url ? 
-										<a href={partner.url} target='_blank' rel='noopenter noreferrer'>
-											{partner.logo ? <img src={partner.logo} alt={partner.name} className='partner_logo' /> : <p>{partner.name}</p>}
-										</a>
-										: 
-										partner.logo ? <img src={partner.logo} alt={partner.name} className='partner_logo' /> : <p>{partner.name}</p>
-									}
-								</ScrollAnimation>
-							</div>
-						);
-					})}
-				</div>
 			</Grid>
 		</section>
 	);
